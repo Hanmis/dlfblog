794c8ce0f77c598075164e0ba5e1513cb7b91f1e
hliu <hliu@hliu-desktop.(none)>
2012-12-06 15:52:50 +0800

    dlfblog ci

---
+++ protected/config/dlfdb.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	'connectionString' => 'mysql:host=localhost;dbname=dlfblog',
+        'emulatePrepare' => true,
+        'username' => 'root',
+        'password' => '6596022',
+        'charset' => 'utf8',
+        'tablePrefix' => 'dlf_',
+);
